e2d0f5224e91cfe7be5747567b6254280d95393e
voyagerwoo <voyagerwoo@Yeomyeongui-MacBook-Pro.local>
2018-02-01 19:16:19 +0900

    add stage

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ejs = require('ejs')
 // lambda proxy integration
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
+    const stage = event.requestContext.stage;
     const path = event.path;
     const filename = `./contents${event.path}/index.ejs`;
     console.log(filename);
@@ -18,7 +19,7 @@ exports.handler = (event, context, callback) => {
                 ["content-type"] : 'text/html'
             }
         });
-        const html = ejs.render(data.toString());
+        const html = ejs.render(data.toString(), {stage});
         callback(null, {
             statusCode : 200,
             body: html,
